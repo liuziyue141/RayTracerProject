0071348cdf52a5d4788e0e74f917bd644c0f9ea2
Ziyue Liu <zil085@ucsd.edu>
2024-03-08 01:02:12 -0800

    finish scene 2

--- raytracer.cpp
+++ raytracer.cpp
@@ -13,6 +13,10 @@ int Intersect(Ray &ray){
     for (int i = 0 ; i < numobjects ; i++) {
         object* obj = &(objects[i]);
         if(obj->type == "sphere"){
+
+            // ray.P0 = vec3(glm::inverse(obj->transform)*vec4(ray.P0, 1.0f));
+            // ray.P1 = vec3(glm::inverse(obj->transform)*vec4(ray.P0, 1.0f));
+        
             float a = glm::dot(ray.P1, ray.P1);
             float b = 2.0f * glm::dot(ray.P1, ray.P0-(obj->sphere_loc));
             float c = glm::dot(ray.P0 - obj->sphere_loc, ray.P0 - obj->sphere_loc) - obj->sphere_rad*obj->sphere_rad;
@@ -28,11 +32,14 @@ int Intersect(Ray &ray){
                     objectId = i;
                 }
             }
+
+            // ray.P0 = vec3(obj->transform*vec4(ray.P0, 1.0f));
+            // ray.P1 = vec3(obj->transform*vec4(ray.P0, 1.0f));
+
         }else{
-            vec3 A = obj->tri_v1;
-            vec3 B = obj->tri_v2;
-            vec3 C = obj->tri_v3;
-            //vec3 normal = glm::cross((B-A), (C-A));//need to verify
+            vec3 A = vec3(obj->transform*vec4(obj->tri_v1, 1.0f));
+            vec3 B = vec3(obj->transform*vec4(obj->tri_v2, 1.0f));
+            vec3 C = vec3(obj->transform*vec4(obj->tri_v3, 1.0f));
             vec3 n = glm::normalize(glm::cross((C-A), (B-A)));
             float t = (glm::dot(A, n)-glm::dot(ray.P0, n))/glm::dot(ray.P1, n);
             if(t<min_t){
@@ -40,14 +47,19 @@ int Intersect(Ray &ray){
                 float det = (C.x - A.x)*(B.y - A.y) - (B.x - A.x)*(C.y - A.y);
                 float factor_beta = (C.x - A.x)*(P.y - A.y) - (P.x - A.x)*(C.y - A.y);
                 float factor_gamma = (B.y - A.y)*(P.x - A.x) - (B.x - A.x)*(P.y - A.y);
+                if(A.x == B.x && B.x == C.x){
+                    det = (C.z - A.z)*(B.y - A.y) - (B.z - A.z)*(C.y - A.y);
+                    factor_beta = (C.z - A.z)*(P.y - A.y) - (P.z - A.z)*(C.y - A.y);
+                    factor_gamma = (B.y - A.y)*(P.z - A.z) - (B.z - A.z)*(P.y - A.y);
+                }else if(A.y == B.y && B.y == C.y){
+                    det = (C.x - A.x)*(B.z - A.z) - (B.x - A.x)*(C.z - A.z);
+                    factor_beta = (C.x - A.x)*(P.z - A.z) - (P.x - A.x)*(C.z - A.z);
+                    factor_gamma = (B.z - A.z)*(P.x - A.x) - (B.x - A.x)*(P.z - A.z);
+                }
                 float beta = factor_beta / det;
                 float gamma = factor_gamma / det;
                 float alpha = 1.0 - gamma - beta;
                 if(alpha>=0 && beta>=0 && gamma >= 0){
-                //                     cout << "beta value: " << beta << endl;
-                // cout << "alpha value: " << alpha << endl;
-                // cout << "gamma value: " << gamma << endl;
-                // cout << endl;
                     min_t = t;
                     objectId = i;
                     P_return = P;
@@ -87,32 +99,21 @@ void traceRay(){
         for(int j = 0; j<h; j++){
             Ray ray = RayThruPixel(i, j);
             int objectId = Intersect(ray);
-            //cout << objectId << endl;
             if(objectId>=0){
-                //cout << "reached this line" << endl;
                 object* obj = &(objects[objectId]);
-                color.rgbRed = (double) obj->diffuse[2]*255.0;
-                //color.rgbRed = 255;
-                color.rgbGreen = (double) obj->diffuse[1]*255.0;
-                //color.rgbGreen = 0;
-                color.rgbBlue = (double) obj->diffuse[0]*255.0;
-                //color.rgbBlue = 0;
-                //pixels[i][j] =0xFF0000;
+                color.rgbRed = (double) obj->ambient[2]*255.0 + obj->diffuse[2]*255.0 *0.5;
+                color.rgbGreen = (double) obj->ambient[1]*255.0 + obj->diffuse[1]*255.0 *0.5;
+                color.rgbBlue = (double) obj->ambient[0]*255.0 + obj->diffuse[0]*255.0 *0.5;
+
             }else{
                 color.rgbRed = 0;
-                //color.rgbGreen = (double) obj->diffuse[1]*255.0;
                 color.rgbGreen = 0;
-                //color.rgbBlue = (double) obj->diffuse[2]*255.0;
                 color.rgbBlue = 0;
-                //pixels[i][j] =0x000000;
-
             }
             FreeImage_SetPixelColor(bitmap, i, h-j-1, &color);
         
         }
     }
-    //FIBITMAP *bitmap = FreeImage_ConvertFromRawBits(pixels[0], w, h, w * 3, 24, 0xFF0000, 0x00FF00, 0x0000FF, true);
-
     if (FreeImage_Save(FIF_PNG, bitmap, "test.png", 0)){
         cout << "Image successfully saved!" << endl;
     }
